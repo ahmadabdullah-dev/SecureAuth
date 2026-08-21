12efd99ee3e18998f697b38449c557dc3fe7a8ca
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-08-21 12:27:42 +0300

    feat(api): Add ErrorCode to result pattern<T>

--- Business/Common/Result.cs
+++ Business/Common/Result.cs
@@ -5,15 +5,19 @@ public class Result<T>
     public bool IsSuccess { get; set; }
     public T? Value { get; set; }
     public string? Error { get; set; }
+    public int ErrorCode { get; set; }
+
 
     public static Result<T> Success(T value) => new()
     {
         IsSuccess = true,
         Value = value
     };
-    public static Result<T> Failure(string error) => new()
+    public static Result<T> Failure(string error, int errorCode) => new()
     {
         IsSuccess = false, 
         Error = error,
+        ErrorCode = errorCode
+
     };
 }
